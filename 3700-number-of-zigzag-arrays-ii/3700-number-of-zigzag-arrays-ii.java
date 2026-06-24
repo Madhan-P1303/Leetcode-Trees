b4d80d9a5c802343e4c0907fc53005a3475fc22b
Madhan P <madhan93454@gmail.com>
2026-06-24 10:59:29 +0530

    Time: 85 ms (93.55%), Space: 46.7 MB (61.29%) - LeetHub

--- 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
@@ -1,105 +1,75 @@
 class Solution {
-    static final long MOD = 1_000_000_007L;
-
     public int zigZagArrays(int n, int l, int r) {
-        int m = r - l + 1;
-
-        if (n == 2) {
-            return (int)((long)m * (m - 1) % MOD);
-        }
-
-        int size = 2 * m;
-
-        long[][] T = new long[size][size];
-
-        // up'[j] = sum down[k], k < j
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < j; k++) {
-                T[j][m + k] = 1;
+        int len = r - l + 1;
+        long[][] m1 = new long[len][len];
+        long[][] m2 = new long[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                m1[i][j] = 1;
+            }
+            for (int j = 0; j < i; j++) {
+                m2[i][j] = 1;
             }
         }
-
-        // down'[j] = sum up[k], k > j
-        for (int j = 0; j < m; j++) {
-            for (int k = j + 1; k < m; k++) {
-                T[m + j][k] = 1;
-            }
+        long[][] m = pro(m1, m2);
+        long[] arr = new long[len];
+        Arrays.fill(arr, 1);
+        n--;
+        int count = n / 2;
+        while (count > 0) {
+            if (count % 2 == 1)
+                arr = pro(arr, m);
+            m = pro(m);
+            count /= 2;
         }
+        if (n % 2 == 1)
+            arr = pro(arr, m1);
+        long res = 0;
+        for (long num : arr) {
+            res += num;
+        }
+        return (int) (res * 2 % mod);
+    }
 
-        long[] base = new long[size];
+    int mod = 1_000_000_007;
 
-        // length = 2
-        for (int a = 0; a < m; a++) {
-            for (int b = 0; b < m; b++) {
-                if (a == b) continue;
-
-                if (b > a) {
-                    base[b]++;
-                } else {
-                    base[m + b]++;
+    public long[][] pro(long[][] a) {
+        long[][] res = new long[a.length][a[0].length];
+        for (int i = 0; i < res.length; i++) {
+            for (int k = 0; k < res.length; k++) {
+                if (a[i][k] == 0)
+                    continue;
+                for (int j = 0; j < res.length; j++) {
+                    res[i][j] = (res[i][j] + a[i][k] * a[k][j]) % mod;
                 }
             }
         }
-
-        long[][] P = power(T, n - 2);
-
-        long ans = 0;
-
-        for (int i = 0; i < size; i++) {
-            long cur = 0;
-
-            for (int j = 0; j < size; j++) {
-                cur = (cur + P[i][j] * base[j]) % MOD;
-            }
-
-            ans = (ans + cur) % MOD;
-        }
-
-        return (int) ans;
-    }
-
-    private long[][] power(long[][] A, long exp) {
-        int n = A.length;
-
-        long[][] res = new long[n][n];
-
-        for (int i = 0; i < n; i++) {
-            res[i][i] = 1;
-        }
-
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                res = multiply(res, A);
-            }
-
-            A = multiply(A, A);
-            exp >>= 1;
-        }
-
         return res;
     }
 
-    private long[][] multiply(long[][] A, long[][] B) {
-        int n = A.length;
-
-        long[][] C = new long[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int k = 0; k < n; k++) {
-
-                if (A[i][k] == 0) continue;
-
-                long val = A[i][k];
-
-                for (int j = 0; j < n; j++) {
-
-                    if (B[k][j] == 0) continue;
-
-                    C[i][j] = (C[i][j] + val * B[k][j]) % MOD;
+    public long[][] pro(long[][] a, long[][] b) {
+        long[][] res = new long[a.length][a[0].length];
+        for (int i = 0; i < res.length; i++) {
+            for (int k = 0; k < res.length; k++) {
+                if (a[i][k] == 0)
+                    continue;
+                for (int j = 0; j < res.length; j++) {
+                    res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % mod;
                 }
             }
         }
+        return res;
+    }
 
-        return C;
+    public long[] pro(long[] a, long[][] b) {
+        long[] res = new long[a.length];
+        for (int j = 0; j < res.length; j++) {
+            if (a[j] == 0)
+                continue;
+            for (int i = 0; i < res.length; i++) {
+                res[i] = (res[i] + a[j] * b[j][i]) % mod;
+            }
+        }
+        return res;
     }
 }

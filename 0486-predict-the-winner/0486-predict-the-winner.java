d07781bb887f51572ef58476e5719efe02d214d9
Madhan P <madhan93454@gmail.com>
2026-08-01 16:07:04 +0530

    Time: 67 ms (19.36%), Space: 42.8 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int difference = solve(nums, 0, nums.length - 1);
+
+        return difference >= 0;
+    }
+
+    private int solve(int[] nums, int left, int right) {
+
+        if (left == right) {
+            return nums[left];
+        }
+
+        int takeLeft =
+            nums[left] - solve(nums, left + 1, right);
+
+        int takeRight =
+            nums[right] - solve(nums, left, right - 1);
+
+        return Math.max(takeLeft, takeRight);
+    }
+}
